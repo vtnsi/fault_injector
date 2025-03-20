fb45c1d86900c5fb9180d2e116d1c5b1ab67012c
Daniel DeCollo <64618012+ddecollo@users.noreply.github.com>
2025-03-20 13:52:27 -0400

    Minor changes (#1)
    * create df fault injection class, update demo and documentation
    
    * remove temporary comment from data_frame_injector
    
    * update start and stop variables: default, random value logic, documentation
    
    * update error handling
    
    * rename 'reset_values' to 'restore_values'
    
    * default params to None
    
    * simplify demo
    
    * update function/class descriptions
    
    * update plot: description and colors
    
    * add **__pycache__ to gitignore file
    
    * update installation description
    
    * update readme installation description

---
+++ fault_injector/data_frame_injector.py
@@ -0,0 +1,59 @@
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import fault_injector.injector as injector
+
+
+class DataFrameFaultInjection():
+    def __init__(self, df:pd.DataFrame, column_names, start_val:int, stop_val:int):
+        """
+        DataFrameFaultInjection is an extension of the FaultInjection class that allows
+            users to inject faults into multiple columns in a dataframe
+
+        Args:
+            df (pd.DataFrame): Pandas DataFrame
+            column_names (list or string): represents either a single column name or a 
+                list of column names in df that will be injected with the fault
+            start_val (int): int value representing the starting index value for the fault
+            stop_val (int): int value representing the last index value for the fault
+        """       
+        self.fault_dict = {}
+        self.original_df = df.copy()
+        self.fault_df = df.copy()
+
+        if isinstance(column_names, str):
+            column_names = [column_names]
+        self.column_names = column_names
+        self.start_val = start_val
+        self.stop_val = stop_val
+
+    def inject_fault(self):
+        """
+        This function creates a dictionary of FaultInjection class objects and stores 
+            them in self.fault_dict. This allows the user to utilize all of the FaultInjection
+            capabilities for their desired columns.
+
+        Returns:
+            self.fault_dict (dict): this dictionary of FaultInjection class objects, 
+                where the keys represent the column names
+        """        
+        for i in self.column_names:
+            original_lst = list(self.original_df[i].values)
+            df_inject = injector.FaultInjection(values=original_lst, start=self.start_val, stop=self.stop_val, increasing=1)
+            
+            self.fault_dict[i] = df_inject
+        return self.fault_dict
+
+    def update_df(self):
+        """
+        This function returns an updated df that now contains the faulty data
+
+        Returns:
+            self.fault_df (pd.DataFrame): this dataframe takes the same format of the original 
+                data. The columns that were injected with faults are now updated with the fault data
+        """        
+        for key in self.fault_dict.keys():
+            self.fault_df[key] = self.fault_dict[key].values
+
+        return self.fault_df
--- fault_injector/injector.py
+++ fault_injector/injector.py
@@ -1,93 +1,91 @@
 import random
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-
-
-def DataFrameFaultInjection(df, column_names, start_val, stop_val):
-    fault_dict = {}
-
-    if isinstance(column_names, str):
-        column_names = [column_names]
-
-    for i in column_names:
-        original_lst = list(df[i].values)
-        df_inject = FaultInjection(values=original_lst, start=start_val, stop=stop_val, increasing=1)
-        
-        fault_dict[i] = df_inject
-    return fault_dict
-
-def update_df(df, fault_dict):
-    fault_df = df.copy()
-    for key in fault_dict.keys():
-        fault_df[key] = fault_dict[key].values
-
-    return fault_df
 
 class FaultInjection:
-    # this class is designed to take in a list of numeric values and inject a fault into the values
-    # values will be a list of numeric values
-    # start will dictate the starting time for the fault. It should be a positive integer that is less than the length of the values
-    # end will dictate the ending time for the fault. It should be an integer that is larger than start, but less than the length of the values
-    # to randomly generate a start/end set the variable to -1
-    # increasing should be a binary value (0 or 1). 1 will cause the fault to increase the values and 0 will decrease the values.
+    def __init__(self, values, start=None, stop=None, increasing=0):
+        """
+        The FaultInjection class is designed to take in a list of numeric values and inject a fault into the values
 
+        Args:
+            values: a list of numeric values
+            start: defines the starting time for the fault and corresponds with the index values for the elements in the list.
+                - It must be a non-negative integer less than the length of the values.
+                - "random" can be used to generate a random starting value.
+                - Defaults to None.
+            stop: defines the ending time for the fault and corresponds with the index values for the elements in the list.
+                - It must be a non-negative integer less than the length of the values and greater than the 'start' value.
+                - 'random' can be used to generate a random ending value.
+                - Defaults to None.
+            increasing: should be a binary value (0 or 1).
+                - 1 will cause the fault to increase the values.
+                - 0 will decrease the values.
+                - Defaults to 0.
+        """
 
-    def __init__(self, values, start=-1, stop=-1, increasing=0):
-        # check that values is a list of numbers
-        if not isinstance(values, list):
-            raise ValueError("values needs to be a list of numbers")
-        elif not isinstance(values[0], (float, int, np.int64)):
-            raise ValueError("values needs to be a list of numbers")
+        try:
+            # Check if it's a non-empty list
+            if not isinstance(values, list) or not values:
+                raise ValueError("Invalid Input (values): \n values must be a non-empty list of numbers.")
 
-        # find the maximum possible length of the fault
-        self.max_end = len(values)
+            # Check the type of the first element in the list
+            if not isinstance(values[0], (float, int, np.int64)):
+                raise ValueError("Invalid Input (values): \n values must be a list of numbers.")
 
-        # this variable will receive the fault injection
-        self.values = values.copy()
+            # find the maximum possible length of the fault
+            self.max_end = len(values)
 
-        # the original values will be preserved and can be used for comparison
-        self.original_values = values.copy()
+            # this variable will receive the fault injection
+            self.values = values.copy()
 
-        if not isinstance(start, int):
-            raise ValueError("start needs to be an integer between 0 and the length of values. Set to -1 if you want the starting point to be random.")
-        elif start >= self.max_end:
-            raise ValueError("start needs to be an integer between 0 and the length of values. Set to -1 if you want the starting point to be random.")
-        elif start < 0:
-            # by setting the value to -1, the class will generate a random starting point
-            self.start = random.randint(0, self.max_end / 2)
-        else:
-            self.start = start
+            # the original values will be preserved and can be used for comparison
+            self.original_values = values.copy()
 
-        if not isinstance(stop, int):
-            raise ValueError("stop needs to be an integer between start and the length of values. Set to -1 if you want the ending point to be random.")
-        elif stop > self.max_end:
-            raise ValueError("start needs to be an integer between start and the length of values. Set to -1 if you want the ending point to be random.")
-        elif stop > 0 and stop < self.start:
-            raise ValueError("start needs to be an integer between start and the length of values. Set to -1 if you want the ending point to be random.")
-        elif stop < 0:
-            # by setting the value to -1, the class will generate a random ending point
-            self.stop = random.randint(self.stop, self.max_end)
-        else:
-            self.stop = stop
+            # calculate the average value
+            self.original_average = np.average(self.original_values)
 
-        # calculate the average value
-        self.original_average = np.average(self.original_values)
+            # Validate 'start'
+            if start is None:
+                self.start = 0
+            elif start == "random":
+                self.start = random.randint(0, self.max_end // 2)
+            elif isinstance(start, int) and start >= 0:
+                self.start = min(start, self.max_end - 1)
+            else:
+                raise ValueError(
+                    "Invalid Input (start): \n It must be either:\n"
+                    " - An integer between 0 and the length of values.\n"
+                    " - 'random' to generate a random starting point.\n"
+                    " - None to default to 0."
+                )
 
-        # define the fault length
-        self.fault_length = self.stop - self.start
+            # Validate 'stop'
+            if stop is None:
+                self.stop = self.max_end
+            elif stop == "random":
+                self.stop = random.randint(self.start + 1, self.max_end)
+            elif isinstance(stop, int) and stop >= 0:
+                self.stop = min(max(self.start, stop), self.max_end)
+            else:
+                raise ValueError(
+                    "Invalid Input (stop): \n 'stop' must be an integer between 'start' and the length of values,\n"
+                    "or 'random' to generate a random endpoint.\n"
+                    "Set to None to default to the length of values."
+                )
 
-        # check if the fault will be increasing
-        if increasing == 1 or increasing == 0:
-            self.fault_direction = 1
-        elif increasing == 0:
-            self.fault_direction = -1
-        else:
-            raise ValueError("increasing needs to be 0 or 1")
+            # Validate 'increasing'
+            if increasing in {0, 1}:
+                self.fault_direction = 1 if increasing == 1 else -1
+            else:
+                raise ValueError("Invalid Input (increasing): \n increasing must be 0 or 1.")
+            # define the fault length
+            self.fault_length = self.stop - self.start
 
+        except ValueError as e:
+            print(f"Error: {e}")
 
-    def reset_values(self):
-        # this will remove the injected fault
+    def restore_values(self):
+        # this will revert values back to the original state
         self.values = self.original_values.copy()
 
     def inject_fault(self, change_lst):
@@ -111,12 +109,15 @@ class FaultInjection:
 
 
     def inject_drift(self, drift_rate=None):
-        if drift_rate == None:
-            # generate a random drift rate
-            drift_rate = random.uniform(0, 0.0001) * self.fault_direction * self.original_average
-        elif not isinstance(drift_rate, (float, int, np.int64)):
-            raise ValueError("drift_rate needs to be a float or int")
-        self.drift_rate = drift_rate
+        try:
+            if drift_rate == None:
+                # generate a random drift rate
+                drift_rate = random.uniform(0, 0.0001) * self.fault_direction * self.original_average
+            elif not isinstance(drift_rate, (float, int, np.int64)):
+                raise ValueError("drift_rate needs to be a float or int")
+            self.drift_rate = drift_rate
+        except ValueError as e:
+            print(f"Error: {e}")
 
         # list of values that equal the amount of change during the fault injection
         change_lst = list(np.array(range(1, self.fault_length + 1)) * self.drift_rate)
@@ -125,11 +126,13 @@ class FaultInjection:
 
 
     def inject_offset(self, offset_rate=None):
-        if offset_rate == None:
-            offset_rate = random.uniform(0.01, 0.1)
-        elif not isinstance(offset_rate, (float, int, np.int64)):
-            raise ValueError("offset_rate needs to be a float or int")
-        
+        try:
+            if offset_rate == None:
+                offset_rate = random.uniform(0.01, 0.1)
+            elif not isinstance(offset_rate, (float, int, np.int64)):
+                raise ValueError("offset_rate needs to be a float or int")
+        except ValueError as e:
+            print(f"Error: {e}")
         self.offset_rate = self.original_average * offset_rate
 
         # list of values that equal the amount of change during the fault injection
@@ -147,71 +150,76 @@ class FaultInjection:
 
     def inject_stuck_value(self, stuck_val=None):
         # check stuck_val input
-        if stuck_val is None:
-            stuck_val = random.uniform(0.01, 0.1) * self.fault_direction * self.original_average + self.original_average
-        elif not isinstance(stuck_val, (int, float, np.int64)):
-            raise ValueError("stuck_val needs to be a float or an int. To generate a random stuck_val, set to None")
-
+        try:
+            if stuck_val is None:
+                stuck_val = random.uniform(0.01, 0.1) * self.fault_direction * self.original_average + self.original_average
+            elif not isinstance(stuck_val, (int, float, np.int64)):
+                raise ValueError("stuck_val needs to be a float or an int. To generate a random stuck_val, set to None")
+        except ValueError as e:
+            print(f"Error: {e}")
         # list of stuck values
         change_lst = [stuck_val] * self.fault_length
 
         self.values[self.start:self.stop] = change_lst
 
 
-    def inject_noise(self, noise_type, params = []):
-        if not isinstance(params, list):
-            raise ValueError("params needs to be a list")
-        elif noise_type == 'gaussian':
-            if len(params) == 0: 
-                # calculate mean
-                params.append(np.array(self.values[self.start:self.stop]).mean())
-                # calculate standard deviation
-                params.append(np.std(self.values[self.start:self.stop]))
-            elif len(params) > 2 or len(params) == 1:
-                raise ValueError("""Params should either be an empty list or it should contain 2 elements. 
-                                 \nThe values will be used in np.random.normal(params[0], params[1], fault_length)
-                                 \nparams[0] typically represents the mean of the distribution
-                                 \nparams[1] typically represents the standard deviation""")
+    def inject_noise(self, noise_type, params=None):
+        if params is None:
+            params = []
+        try:
+            if not isinstance(params, list):
+                raise ValueError("params needs to be a list")
+            elif noise_type == 'gaussian':
+                if len(params) == 0:
+                    # calculate mean
+                    params.append(np.array(self.values[self.start:self.stop]).mean())
+                    # calculate standard deviation
+                    params.append(np.std(self.values[self.start:self.stop]))
+                elif len(params) > 2 or len(params) == 1:
+                    raise ValueError("""Params should either be an empty list or it should contain 2 elements.
+                                    \nThe values will be used in np.random.normal(params[0], params[1], fault_length)
+                                    \nparams[0] typically represents the mean of the distribution
+                                    \nparams[1] typically represents the standard deviation""")
 
-            change_lst = np.random.normal(params[0], params[1], self.fault_length)
-            self.values[self.start:self.stop] = change_lst
+                change_lst = np.random.normal(params[0], params[1], self.fault_length)
+                self.values[self.start:self.stop] = change_lst
 
-        elif noise_type == 'uniform':
-            if len(params) == 0: 
-                # calculate min value
-                params.append(0.999 * self.original_average)
-                # calculate max value
-                params.append(1.001 * self.original_average)
-            elif len(params) > 2 or len(params) == 1:
-                raise ValueError("""Params should either be an empty list or it should contain 2 elements. 
-                                 \nThe values will be used in np.random.uniform(params[0], params[1], fault_length)
-                                 \nparams[0] represents the lower bound
-                                 \nparams[1] represents the upper bound""")
+            elif noise_type == 'uniform':
+                if len(params) == 0:
+                    # calculate min value
+                    params.append(0.999 * self.original_average)
+                    # calculate max value
+                    params.append(1.001 * self.original_average)
+                elif len(params) > 2 or len(params) == 1:
+                    raise ValueError("""Params should either be an empty list or it should contain 2 elements.
+                                    \nThe values will be used in np.random.uniform(params[0], params[1], fault_length)
+                                    \nparams[0] represents the lower bound
+                                    \nparams[1] represents the upper bound""")
 
-            change_lst = np.random.uniform(params[0], params[1], self.fault_length)
-            self.values[self.start:self.stop] = change_lst
-
-        else:
-            raise ValueError('noise_type needs to be \"gaussian\" or \"uniform\" ')
-
-
-
-
+                change_lst = np.random.uniform(params[0], params[1], self.fault_length)
+                self.values[self.start:self.stop] = change_lst
+            else:
+                raise ValueError('noise_type needs to be \"gaussian\" or \"uniform\" ')
+        except ValueError as e:
+            print(f"Error: {e}")
 
     def plot_compare_values(self):
-        # this function can be used to plot the injected fault and compar it to the original data
+        """
+        this function can be used to plot the injected fault and compar it to the original data
+        """
         # calculate the middle of the original y values
         y_mid =  (max(self.original_values) - min(self.original_values)) / 2 + min(self.original_values)
 
         fig, ax = plt.subplots()
-        # plot the injected values
-        ax.plot(self.values, color="blue")
 
         # plot the original values
-        ax.plot(self.original_values, color="blue", alpha=0.2)
+        ax.plot(self.original_values, color='#1f77b4', label="Original")
+
+        # plot the injected values
+        ax.plot(self.values, color='#d62728', label="Fault")
 
         # plot a horizontal line in the middle of the original values
-        ax.hlines(y=0.50, xmin=0, xmax=len(self.values), color="black")
+        ax.hlines(y=y_mid, xmin=0, xmax=len(self.values), color="black")
 
         #plot a vertical line where the fault starts
         ax.vlines(
@@ -233,6 +241,6 @@ class FaultInjection:
         # dynamically change the y limits based on the values
         ax.set_ylim([min(min(self.original_values), min(self.values)) * 0.995, max(max(self.original_values), max(self.values)) * 1.005])
         ax.grid()
-
+        plt.legend()
         plt.show()
 
