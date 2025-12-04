a87d9c092d00e74481b9bfb91c5488073536933e
ddecollo <ddecollo@vt.edu>
2025-12-04 15:54:09 +0000

    Uniform noise fault update

---
+++ fault_injector/fault_lib/uniform_noise_fault.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+uniform noise fault class
+"""
+import numpy as np
+from fault_injector.fault_lib.base_fault import BaseFault
+from numbers import Number
+
+class UniformNoiseFault(BaseFault):
+    def __init__(self, params:dict = None):
+        r"""
+        UniformNoiseFault
+        ----------
+
+        Args
+        ----
+        params (dict):
+        - min_val (numeric): Mean of the Gaussian noise distribution
+        - max_val (numeric): Standard deviation of the Gaussian noise distribution. Must be non-negative.
+
+
+        """
+        self.name = 'uniform_noise_fault'
+
+        if params is None:
+            # set default values for params
+            params = {'min_val': 0,
+                      'max_val': 1}
+
+        self.min_val = params.get('min_val')
+        self.max_val = params.get('max_val')
+        self._check_params()
+
+
+    def __call__(self, x:np.array)->np.ndarray:
+        """The call method generates the uniform noise fault
+
+        Args:
+            x (np.array): array containing numeric values that represent the original value
+
+        Returns:
+            np.ndarray: array containing the altered values
+        """
+        self._check_params()
+        self._check_data_type(x)
+
+        noise =  np.random.uniform(self.min_val, self.max_val, len(x))
+        return x + noise
+
+
+    def _check_params(self):
+        """
+        Checks the params
+        - min_val: must be numeric
+        - max_val: must be numeric
+
+        Raises:
+            ValueError: param is None
+            ValueError: param needs to be a numeric value
+        """
+        # min_val checks
+        if self.min_val is None:
+            raise ValueError(f"Invalid 'min_val': \n no min_val set in params")
+        elif not isinstance(self.min_val, (Number, np.number)):
+            raise ValueError(f"Invalid 'min_val': \n must be a numeric type (float, int, np.int64, np.float32, np.float64, np.int32, etc.).")
+
+        # max_val checks
+        if self.max_val is None:
+            raise ValueError(f"Invalid 'max_val': \n no max_val set in params")
+        elif not isinstance(self.max_val, (Number, np.number)):
+            raise ValueError(f"Invalid 'max_val': \n must be a numeric type (float, int, np.int64, np.float32, np.float64, np.int32, etc.).")
+        elif self.max_val <= self.min_val:
+            raise ValueError(f"Invalid 'max_val': \n must be greater than min_val")
+
+
+    def _check_data_type(self, x:np.array):
+        """
+        Check that x is an array containing numeric values
+
+        Args:
+            x (np.ndarray): array containing numeric values that represent the original value
+
+        Raises:
+            ValueError: 'x' must be an array
+            ValueError: 'x' must contain numeric values
+
+        """
+
+        if not isinstance(x, np.ndarray):
+            raise ValueError(f"Invalid 'x': \n must be an np.ndarray")
+        elif not np.issubdtype(x.dtype, np.number):
+            raise ValueError(f"Invalid 'x': \n must contain numeric values")
